ea694babef8ae825a7b40164673ef317d7aab8b2
Ashley <ashleymsutton@outlook.com>
2016-08-01 14:22:34 -0400

    added a uniqueness validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :friends, :through => :friendships
 
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :password, confirmation: true, presence: true, length: { in: 6..20 }
+  validates :password_confirmation, presence: true, length: { in: 6..20 }
+
 
   def is_friends_with?(user)
     Friendship.exists?(self, user)
