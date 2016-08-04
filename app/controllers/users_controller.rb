a7a62b603ee0b085bb99c35092533818a176b0e8
Deronia Phillips <dlp_md@hotmail.com>
2016-08-03 20:07:01 -0400

    Minor frontend changes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to user_path(current_user)
     else
       redirect_to '/signup'
     end
