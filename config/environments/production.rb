2bc7a6edce6318bbe6fb09c1d8b12de579c5e433
Henry Arbolaez <henryarbolaez@gmail.com>
2016-08-24 15:51:33 -0400

    Update production rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,13 +4,16 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :url => ":s3_domain_url",
+    :path => ":class/:id.:style.:extension",
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :s3_region => ENV['AWS_REGION']
-    }
+      :s3_region => ENV['AWS_REGION'],
+      :bucket => ENV['S3_BUCKET_NAME']
+    },
+    :bucket => ENV['S3_BUCKET_NAME']
   }
+
 
   # config.paperclip_defaults = {
   #   storage: :s3,
