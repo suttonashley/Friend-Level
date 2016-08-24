b091a6f22ef6dc1ea923adcc54aec953eba442ce
Henry Arbolaez <henryarbolaez@gmail.com>
2016-08-24 15:39:27 -0400

    Remove default image from model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ApplicationRecord
       small: "160x160",
       medium: "200x200>",
       thumb: "100x100>"
-      }, default_url: "/images/:style/missing.png"
+      }
+      # , default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
