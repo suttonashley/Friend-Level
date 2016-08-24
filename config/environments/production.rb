445e31e1bbc0a311923bd5f26877e9ac3d18a3ac
Henry Arbolaez <henryarbolaez@gmail.com>
2016-08-24 15:17:07 -0400

    Update production paperclip S3 setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,26 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-
-
   config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+    :storage => :s3,
+    :url => ":s3_domain_url",
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_region => ENV['AWS_REGION']
     }
   }
+
+  # config.paperclip_defaults = {
+  #   storage: :s3,
+  #   s3_credentials: {
+  #     bucket: ENV.fetch('S3_BUCKET_NAME'),
+  #     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+  #     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+  #     s3_region: ENV.fetch('AWS_REGION'),
+  #   }
+  # }
 
   # Code is not reloaded between requests.
   config.cache_classes = false
