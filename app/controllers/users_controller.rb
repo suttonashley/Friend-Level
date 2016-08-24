a764d3e1462cff340f1f56bc945c5ff2284b9a22
Ashley <ashleymsutton@outlook.com>
2016-08-24 10:12:18 -0400

    made changes to application.rb and changed the way user's are saved after creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
+    user.save!
+    if user.save!
       session[:user_id] = user.id
       redirect_to user_path(current_user)
     else
